7db12ee753b1b4d20e16eeeef8758560b441fcc5
Lucas Pimentel <137460003+LucasPimentel91@users.noreply.github.com>
2024-10-29 14:00:10 +0000

    primeiro exercício da lista

---
+++ IN008_ProgramacaoOO/Exercicios_avulsos/listExercicesPOO.java
@@ -0,0 +1,17 @@
+//import java.lang;
+
+public class listExercicesPOO{
+    public static void main(String args[]){
+        //Basic Questions 
+        //1º
+        int a, b, c;
+        a=2;b=3;c=1;
+        double delta=0;
+        delta = Math.pow(delta, 2);
+        delta -= 4*a*c;
+        double x1, x2;
+        x1 = (-b + Math.sqrt(delta)) / 2*a;
+        x2 = (-b - Math.sqrt(delta)) / 2*a;
+        System.out.println("As raízes são x1 =" + x1 + "e x2 = " + x2);
+    }
+}
