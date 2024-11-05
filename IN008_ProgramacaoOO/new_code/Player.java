2cf1f2c7db5d135e522fad2737eb956dd3647505
Lucas Pimentel <137460003+LucasPimentel91@users.noreply.github.com>
2024-11-05 21:18:43 +0000

    class Player corrected and tested

--- IN008_ProgramacaoOO/new_code/Player.java
+++ IN008_ProgramacaoOO/new_code/Player.java
@@ -16,30 +16,45 @@ public class Player {
         this.energy= x;
     }
     
-    public void decreaseEnergy(int x){
-        this.energy= x;
-    }
-
-    public void haveSword(){
-        if(this.energy >= this.Min_energy){
-            this.sword=true;
-        }
-        this.sword= false;
-    }
-
-    public void power(){
-        if(this.sword == true){
-            this.energy *= 10;
-        }
-    }
 
     public void duel(Player duelist){
+        while(true){
+        this.energy = ((this.sword==false && this.energy >= this.Min_energy) ? this.energy *= 10 : this.energy);
+        duelist.energy = ((duelist.sword==false && duelist.energy >= duelist.Min_energy) ? duelist.energy *= 10 : duelist.energy);
         if(duelist.energy > this.energy){
-            this.energy--;
             duelist.energy++;
+            this.energy--;
         }
-        this.energy++;
-        duelist.energy--;
+        else{
+            duelist.energy--;
+            this.energy++;
+        }
+
+        if(this.energy <= 0 || duelist.energy <= 0){
+            if(this.energy <= 0){
+                System.out.print(this.name + "ganhou");
+                break;
+            }
+            else{
+                System.out.print(duelist.name + "ganhou");
+                break;
+            }
+
+        }
+
+
+    }
+
+}
+
+    public static void main(String [] args){
+        Player player1 = new Player("Lucas", 10);
+        Player player2 = new Player("John", 10);
+
+        player1.increaseEnergy(50);
+        player2.increaseEnergy(50);
+
+
     }
     
 }
