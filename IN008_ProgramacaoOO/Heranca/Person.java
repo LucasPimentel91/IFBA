8cf9755e7957afa332134f4df194f79879e9f8ca
Lucas Pimentel <137460003+LucasPimentel91@users.noreply.github.com>
2024-11-12 23:37:21 +0000

    11/11/2024

--- IN008_ProgramacaoOO/Heranca/Person.java
+++ IN008_ProgramacaoOO/Heranca/Person.java
@@ -6,6 +6,13 @@ class Person
     private int age;
     protected char sexo;
 
+    public Person(String name, int id, String address, int age){
+        this.name = name;
+        this.id = id;
+        this.address = address;
+        this.age=age;
+    }
+
     public void becomeOlder(){
         this.age++;
         System.out.println("Idade: " + this.age);
