cfd7df7f72abd79873dd53f9182b63ae2683de5f
placte40@gmail.com <placte40@gmail.com>
2024-11-28 21:12:04 -0300

    Problemas para inicializar objetos para serilizar. Focar nas outras etapas para não atrasar.

--- IN008_ProgramacaoOO/T1/main/Main.java
+++ IN008_ProgramacaoOO/T1/main/Main.java
@@ -5,20 +5,40 @@ import models.User;
 import models.Order;
 import models.Product;
 import models.ShoppingCart;
+import java.util.*;
 import java.io.*;
 
 public class Main{
+    Scanner scan = new Scanner(System.in);
+    /*scan.close();
+
+    FileOutputStream fileCreation = new FileOutputStream("config/database.bytej");
+
     ObjectOutputStream objectOUT = new ObjectOutputStream(new FileOutputStream("config/database.bytej"));
-    /*objectOUT.writeObject(-- objeto instanciado--);
+    objectOUT.writeObject(-- objeto instanciado--);
     objectOUT.close();
-    */
+    
 
     ObjectInputStream objectIN = new ObjectInputStream(new FileInputStream("config/database.bytej"));
-    /*[Classe] [nome_objeto] = (Classe) objectIN.readObject();
+    [Classe] [nome_objeto] = (Classe) objectIN.readObject();
     object.IN.close();
      */
     
+    public void login(){
+        System.out.println("email: ");
+        String email = scan.nextLine();
+        System.out.println("senha: ");
+        String senha = scan.nextLine();
 
+    }
+
+    public static void main(String[] args){
+        FileOutputStream fileOUT = new FileOutputStream("C://Users//plact//OneDrive//Documentos//scripts//IFBA//IN008_ProgramacaoOO//T1//config//database.bytej");
+        ObjectOutputStream objectOUT = new ObjectOutputStream(fileOUT);
+
+        
+    
+    }
     
 
 }
